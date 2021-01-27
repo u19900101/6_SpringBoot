3c310938d78e18a52e612f67e01a9dc4f4089924
u19900101 <815000342@qq.com>
2021-01-27 20:17:08 +0800

    profile 指定激活环境

--- p1_helloworld_quick/src/main/java/ppppp/config/MyConfig.java
+++ p1_helloworld_quick/src/main/java/ppppp/config/MyConfig.java
@@ -14,7 +14,7 @@ public class MyConfig {
     @Bean
     //将方法的返回值添加到容器中；容器中这个组件默认的id就是方法名
     public HelloService helloService(){
-        System.out.println("使用@Bean将方法加入ioc中");
+        // System.out.println("使用@Bean将方法加入ioc中");
         return new HelloService();
     }
 }
