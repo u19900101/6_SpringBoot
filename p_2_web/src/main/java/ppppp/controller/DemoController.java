be926bf5408bbf291d96f1d7d571b92a2991eb5a
u19900101 <815000342@qq.com>
2021-03-25 09:38:14 +0800

    开启rest风格--mvc:hiddenmethod:filter: enabled: true

--- p_2_web/src/main/java/ppppp/controller/DemoController.java
+++ p_2_web/src/main/java/ppppp/controller/DemoController.java
@@ -1,6 +1,7 @@
 package ppppp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,4 +14,21 @@ public class DemoController {
     public String demo(){
         return "memeda";
     }*/
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String get(){
+        return "get method";
+    }
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String post(){
+        return "post method";
+    }
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String del(){
+        return "del method";
+    }
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String put(){
+        return "put method";
+    }
 }
