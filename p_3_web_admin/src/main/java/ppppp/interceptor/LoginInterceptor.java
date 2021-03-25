091fcd876b3ad9467509d912410877629b0ac9f9
u19900101 <815000342@qq.com>
2021-03-25 22:02:27 +0800

    文件上传

--- p_3_web_admin/src/main/java/ppppp/interceptor/LoginInterceptor.java
+++ p_3_web_admin/src/main/java/ppppp/interceptor/LoginInterceptor.java
@@ -7,6 +7,7 @@ package ppppp.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,16 +22,34 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-
         log.info("拦截的请求路径为{}: ",request.getRequestURI());
         Object loginUser = request.getSession().getAttribute("loginUser");
         // 放行的判断
         if(loginUser != null){
             return true;
         }
-        request.getSession().setAttribute("msg", "请登录...");
-        response.sendRedirect("/");
+        // request.getSession().setAttribute("msg", "请登录...");
+        // response.sendRedirect("/");
+        request.setAttribute("msg", "请登录...");
+        request.getRequestDispatcher("/").forward(request, response);
         return false;
     }
+
+    /**
+     * 目标方法执行完成以后
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle执行{}",modelAndView);
+    }
+
+    /**
+     * 页面渲染以后
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("afterCompletion执行异常{}",ex);
+    }
+
+
 }
