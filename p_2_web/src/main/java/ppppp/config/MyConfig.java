4c3fb518b9c5b7f4ae58622157aa82cc5bc02714
u19900101 <815000342@qq.com>
2021-03-25 15:13:15 +0800

    开启 矩阵变量的功能  WebMvcConfigurer定制化SpringMVC的功能

---
+++ p_2_web/src/main/java/ppppp/config/MyConfig.java
@@ -0,0 +1,37 @@
+package ppppp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+
+/**
+ * @author lppppp
+ * @create 2021-03-25 12:19
+ */
+@Configuration
+public class MyConfig {
+    // 自定义 rest 风格的 解析名称 <input name = "_m" value = "DELETE"/>
+    /*@Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        HiddenHttpMethodFilter methodFilter = new HiddenHttpMethodFilter();
+        methodFilter.setMethodParam("_m");
+        return methodFilter;
+    }
+*/
+    //1、开启 矩阵变量的功能  WebMvcConfigurer定制化SpringMVC的功能
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void configurePathMatch(PathMatchConfigurer configurer) {
+                UrlPathHelper urlPathHelper = new UrlPathHelper();
+                // 不移除；后面的内容。矩阵变量功能就可以生效
+                urlPathHelper.setRemoveSemicolonContent(false);
+                configurer.setUrlPathHelper(urlPathHelper);
+            }
+        };
+    }
+}
+
--- p_2_web/src/main/java/ppppp/controller/DemoController.java
+++ p_2_web/src/main/java/ppppp/controller/DemoController.java
@@ -1,13 +1,18 @@
 package ppppp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author lppppp
  * @create 2021-03-24 22:02
  */
+// @Controller
 @RestController
 public class DemoController {
    /* @RequestMapping("/bg-02.jpg")
@@ -15,20 +20,60 @@ public class DemoController {
         return "memeda";
     }*/
 
-    @RequestMapping(value = "/user",method = RequestMethod.GET)
-    public String get(){
-        return "get method";
+    //  rest 风格
+  //   @GetMapping("/user")
+  //   // @RequestMapping(value = "/user",method = RequestMethod.GET)
+  //   public String get(){
+  //       return "get method";
+  //   }
+  //
+  //   @PostMapping("/user")
+  //   // @RequestMapping(value = "/user",method = RequestMethod.POST)
+  //   public String post(){
+  //       return "post method";
+  //   }
+  //
+  //   @DeleteMapping("/user")
+  //   // @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+  //   public String del(){
+  //       return "del method";
+  //   }
+  //
+  //   @PutMapping("/user")
+  //   // @RequestMapping(value = "/user",method = RequestMethod.PUT)
+  //   public String put(){
+  //       return "put method";
+  //   }
+
+
+    @RequestMapping("/req")
+    public String req(HttpServletRequest req){
+        req.setAttribute("msg", "msg");
+        return "forward:success";
     }
-    @RequestMapping(value = "/user",method = RequestMethod.POST)
-    public String post(){
-        return "post method";
+
+
+    @RequestMapping("/success")
+    public void success(HttpServletRequest req,@RequestAttribute("msg") String msg){
+
+        System.out.println(req.getAttribute("msg"));
+        System.out.println(msg);
     }
-    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
-    public String del(){
-        return "del method";
-    }
-    @RequestMapping(value = "/user",method = RequestMethod.PUT)
-    public String put(){
-        return "put method";
+// 矩阵变量
+    //1、语法： 请求路径：/cars/sell;low=34;brand=byd,audi,yd
+    //2、SpringBoot默认是禁用了矩阵变量的功能
+    //      手动开启：原理。对于路径的处理。UrlPathHelper进行解析。
+    //              removeSemicolonContent（移除分号内容）支持矩阵变量的
+    //3、矩阵变量必须有url路径变量才能被解析
+    @GetMapping("/cars/{path}")
+    public Map carsSell(@MatrixVariable("low") Integer low,
+                        @MatrixVariable("brand") List<String> brand,
+                        @PathVariable("path") String path){
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("low",low);
+        map.put("brand",brand);
+        map.put("path",path);
+        return map;
     }
 }
