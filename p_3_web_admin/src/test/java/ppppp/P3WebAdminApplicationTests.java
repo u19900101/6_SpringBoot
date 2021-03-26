33d8e7d9e925c73065f61562cadcb44d544a2cf6
u19900101 <815000342@qq.com>
2021-03-26 16:02:19 +0800

    操作数据库

--- p_3_web_admin/src/test/java/ppppp/P3WebAdminApplicationTests.java
+++ p_3_web_admin/src/test/java/ppppp/P3WebAdminApplicationTests.java
@@ -1,13 +1,21 @@
 package ppppp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class P3WebAdminApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
     @Test
     void contextLoads() {
+        Long aLong = jdbcTemplate.queryForObject("select count(*) from t_label", Long.class);
+        log.info("测试记录数查询：{}",aLong);
     }
 
 }
