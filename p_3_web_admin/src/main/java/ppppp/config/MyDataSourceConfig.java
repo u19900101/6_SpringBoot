94372c5f21916a4a4ef55aac7b1dcbd630d5ba91
u19900101 <815000342@qq.com>
2021-03-26 22:02:41 +0800

    druid开启各种监控

---
+++ p_3_web_admin/src/main/java/ppppp/config/MyDataSourceConfig.java
@@ -0,0 +1,71 @@
+package ppppp.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+/**
+ * @author lppppp
+ * @create 2021-03-26 16:43
+ */
+@Configuration
+public class MyDataSourceConfig {
+
+    // 默认的自动配置是判断容器中没有才会配@ConditionalOnMissingBean(DataSource.class)
+   @ConfigurationProperties("spring.datasource")
+   @Bean
+    public DataSource dataSource() throws SQLException {
+        DruidDataSource druidDataSource = new DruidDataSource();
+
+//        druidDataSource.setUrl();
+//        druidDataSource.setUsername();
+//        druidDataSource.setPassword();
+        //加入监控功能 和 防火墙
+       // druidDataSource.setFilters("stat,wall");
+       // druidDataSource.setFilters("wall");
+
+//        druidDataSource.setMaxActive(10);
+        return druidDataSource;
+    }
+
+    /**
+     * 配置 druid的监控页功能
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        StatViewServlet statViewServlet = new StatViewServlet();
+        ServletRegistrationBean<StatViewServlet> registrationBean = new ServletRegistrationBean<>(statViewServlet, "/druid/*");
+
+        registrationBean.addInitParameter("loginUsername","kk");
+        registrationBean.addInitParameter("loginPassword","kk");
+
+
+        return registrationBean;
+    }
+
+    /**
+     * WebStatFilter 用于采集web-jdbc关联监控的数据。
+     */
+   @Bean
+    public FilterRegistrationBean webStatFilter(){
+        WebStatFilter webStatFilter = new WebStatFilter();
+
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>(webStatFilter);
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+
+        return filterRegistrationBean;
+    }
+
+}
--- p_3_web_admin/src/main/java/ppppp/controller/SqlController.java
+++ p_3_web_admin/src/main/java/ppppp/controller/SqlController.java
@@ -1,27 +1,31 @@
-package ppppp;
+package ppppp.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
 
+/**
+ * @author lppppp
+ * @create 2021-03-26 21:19
+ */
 @Slf4j
-@SpringBootTest
-class P3WebAdminApplicationTests {
-
+@RestController
+public class SqlController {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     @Autowired
     DataSource dataSource;
 
-    @Test
-    void contextLoads() {
+   @GetMapping("/sql")
+    public String  slqDemo() {
         Long aLong = jdbcTemplate.queryForObject("select count(*) from t_label", Long.class);
         log.info("测试记录数查询：{}",aLong);
+        return "kkkkk";
     }
 
 }
