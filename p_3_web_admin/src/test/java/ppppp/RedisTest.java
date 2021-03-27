7a293ba0cbc20e7b10039e093ded53a396f64b48
u19900101 <815000342@qq.com>
2021-03-27 22:15:07 +0800

    redis init

---
+++ p_3_web_admin/src/test/java/ppppp/RedisTest.java
@@ -0,0 +1,40 @@
+package ppppp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lppppp
+ * @create 2021-03-27 20:09
+ */
+@Slf4j
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+
+
+
+    @Test
+    void testRedis(){
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+
+        operations.set("hello2","world");
+
+        String hello = operations.get("hello");
+        System.out.println(hello);
+
+        System.out.println(redisConnectionFactory.getClass());
+    }
+}
